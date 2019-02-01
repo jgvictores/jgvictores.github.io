b3755080940a019fc99ee816656c225bab1f7f49
jgvictores <jcgvicto@gmail.com>
2019-02-01 13:26:58 +0100

    update run.sh, fix links

--- cv/run.sh
+++ cv/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cp ~/My\ Collection.bib victores.bib 
 cat morante2016thesis.bib >> victores.bib
+sed -i "s/{\\\_}/_/g" victores.bib
 pdflatex JuanGVictoresCV.tex
 pdflatex JuanGVictoresCV.tex
 bibtex JuanGVictoresCV.aux
 pdflatex JuanGVictoresCV.tex
 pdflatex JuanGVictoresCV.tex
-
