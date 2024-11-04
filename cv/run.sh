9c8cc1837e8184d07c95d1ebc80100f45f0e6835
jgvictores <jcgvicto@gmail.com>
2024-11-04 13:10:09 +0100

    mendeley-reference-manager-2.125.0-x86_64.AppImage

--- cv/run.sh
+++ cv/run.sh
@@ -7,6 +7,7 @@ cp ~/export.bib victores.bib
 sed -i '/   note = {/d' victores.bib  # sed -i "s/%2F/2F/g" victores.bib
 sed -i "s/@inbook/@incollection/g" victores.bib
 sed -i "s/@article{victores2014thesis/@phdthesis{victores2014thesis/g" victores.bib
+sed -i "s/booktitle/journal/g" victores.bib
 #sed -i "s/@patent/@misc/g" victores.bib
 pdflatex JuanGVictoresCV.tex
 pdflatex JuanGVictoresCV.tex
